e0b2a6bdc38779da780aaf9c4e7a9938cdcb2143
Kaviya <kaviya.r2022cse@sece.ac.in>
2024-11-01 22:11:20 +0530

    route structure

--- main.js
+++ main.js
@@ -6,7 +6,22 @@ app.get('/',(req,res) => {
     res.json({msg: "cybertechninja" });
 });
 
+// crud functions
+app.get('/trackno', () => {
 
+})
+
+app.post('/trackno', () => {
+
+})
+
+app.put('/trackno/:id', () =>{
+
+})
+
+app.delete('/trackno/:id',() => {
+    
+})
 app.listen(port, () => {
     console.log(`CyberTechNinja is running on the server http://localhost:${port}`);
 });
